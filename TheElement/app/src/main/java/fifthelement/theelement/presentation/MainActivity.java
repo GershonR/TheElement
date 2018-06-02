7eff596078850ca0dc7a0ac951f501094ea11c92
Jarde01 <j_arde@live.ca>
2018-06-02 11:59:50 -0500

    Initial project setup
    - added folders for heirarchy
    - added testing folders

---
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package fifthelement.theelement.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fifthelement.theelement.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ TheElement/app/src/test/java/fifthelement/theelement/AllTests.java
@@ -0,0 +1,17 @@
+package fifthelement.theelement;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+// Example import
+//import fifthelement.theelement.application.ApplicationTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        //Add tests here
+        // Ex
+        // ApplicationTest.class
+})
+
+public class AllTests {
+}

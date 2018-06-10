f9eb33ac1faa0a00f168c72f8d7d1871d4a10124
AdamB <bouttela@myumanitoba.ca>
2018-06-09 22:55:11 -0500

    Feature/#19/Story/#22-Adam-Playing-Music:
    - Reverted MainActivity to original state

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/MainActivity.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/MainActivity.java
@@ -1,11 +1,6 @@
 package fifthelement.theelement.presentation;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,18 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.MediaController;
 
 import fifthelement.theelement.R;
-import fifthelement.theelement.presentation.MusicService.MusicBinder;
 
-public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
+public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
-    private MusicService musicService;
-    private MusicController musicController;
-    private Intent playIntent;
-    private boolean musicBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,8 +72,6 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        setMusicController();
     }
 
     @Override
@@ -95,118 +82,5 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private ServiceConnection musicConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            MusicBinder binder = (MusicBinder)service;
-            //get service
-            musicService = binder.getService();
-            musicBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            musicBound = false;
-        }
-    };
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(playIntent == null){
-            playIntent = new Intent(this, MusicService.class);
-            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
-            startService(playIntent);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        stopService(playIntent);
-        musicService = null;
-        super.onDestroy();
-    }
-
-    private void setMusicController(){
-        //Helper method to set up music controller multiple times
-        musicController = new MusicController(this);
-        musicController.setPrevNextListeners(null, null); //TODO: Add play next/prev functionality
-        musicController.setMediaPlayer(this);
-        musicController.setAnchorView(findViewById(R.id.drawer_layout));
-        musicController.setEnabled(true);
-        musicController.show(0);
-    }
-
-    @Override
-    public void start() {
-        musicService.resumePlayer();
-    }
-
-    @Override
-    public void pause() {
-        musicService.pausePlayer();
-    }
-
-    @Override
-    public int getDuration() {
-        if(musicService != null && musicBound && musicService.isPlaying()){
-            return musicService.getSongDuration();
-        }
-        else {
-            return 0;
-        }
-    }
-
-    @Override
-    public int getCurrentPosition() {
-        if(musicService != null && musicBound && musicService.isPlaying()){
-            return musicService.getSongPosition();
-        }
-        else {
-            return 0;
-        }
-    }
-
-    @Override
-    public void seekTo(int pos) {
-        musicService.seekTo(pos);
-    }
-
-    @Override
-    public boolean isPlaying() {
-        if(musicService != null && musicBound){
-            return musicService.isPlaying();
-        }
-        else {
-            return false;
-        }
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        return 0;
-    }
-
-    @Override
-    public boolean canPause() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return true;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return 0;
     }
 }

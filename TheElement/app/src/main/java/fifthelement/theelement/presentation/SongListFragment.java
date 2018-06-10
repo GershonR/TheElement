aa5d894db41bdb0d75c6c98856343548d45305f2
Navjot Dhillon <navjot@Navjots-MBP.hitronhub.home>
2018-06-09 20:32:55 -0500

    Song list view done

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/SongListFragment.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/SongListFragment.java
@@ -5,22 +5,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
+
+import java.util.ArrayList;
 
 import fifthelement.theelement.R;
+import fifthelement.theelement.objects.Album;
+import fifthelement.theelement.objects.Author;
+import fifthelement.theelement.objects.Song;
+import fifthelement.theelement.objects.SongsListAdapter;
 
 public class SongListFragment extends Fragment {
-
-    TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         System.out.println("Im in a SongList Fragment Page");
+
         View view = inflater.inflate(R.layout.song_list_fragment, container, false);
-        textView = view.findViewById(R.id.song_list_text_example);
-        textView.setText("Example Text Here");
+
+        String[] songName = {"Nice For What", "God's Plan", "This Is America", "Yes Indeed", "No Tears Left To Cry"};
+        String[] authorNames = {"Drake", "Drake", "Childish Gambino", "Lil Baby & Drake", "Ariana Grande" };
+        ArrayList<Song> songs = new ArrayList<Song>();
+        for(int i = 0; i < 12; i++) {
+            Song song = new Song(i, songName[i%5]);
+            song.addAlbum(new Album(i, songName[i%5]));
+            song.addAuthor(new Author(i, authorNames[i%5]));
+            songs.add(song);
+        }
+
+        ListView listView = (ListView) view.findViewById(R.id.song_list_view);
+        SongsListAdapter songListAdapter = new SongsListAdapter(getActivity(), songs);
+        listView.setAdapter(songListAdapter);
         return view;
     }
 }

94af948ab6dcfec191d4bf9a1dee1b6de84b7a68
AdamB <bouttela@myumanitoba.ca>
2018-06-10 11:40:23 -0500

    Feature/#19/Story/#22-Adam-Playing-Music:
    - Deleted deprecated MusicController class
    - Added MusicService binding code back into MainActivity, only need to attach MediaController widget and test playback

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/MainActivity.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/MainActivity.java
@@ -1,7 +1,12 @@
 package fifthelement.theelement.presentation;
 
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,14 +17,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.MediaController;
 
 import fifthelement.theelement.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
     private NavigationView nvDrawer;
+
+    //Music playing/controls
+    private MediaController mediaController;
+    private MusicService musicService;
+    private Intent musicIntent;
+    private boolean musicBound = false;
 
     // Make sure to be using android.support.v7.app.ActionBarDrawerToggle version.
 // The android.support.v4.app.ActionBarDrawerToggle has been deprecated.
@@ -45,7 +57,34 @@ public class MainActivity extends AppCompatActivity {
         // Setup our drawer view
         nvDrawer = (NavigationView) findViewById(R.id.nvView);
         setupDrawerContent(nvDrawer);
+
+        setUpMediaController();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(musicIntent == null){
+            musicIntent = new Intent(this, MusicService.class);
+            bindService(musicIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(musicIntent);
+        }
+    }
+
+    //We bind main activity to music service that will run in background
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            musicService = binder.getService();
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -102,5 +141,90 @@ public class MainActivity extends AppCompatActivity {
         setTitle(menuItem.getTitle());
         // Close the navigation drawer
         mDrawer.closeDrawers();
+    }
+
+    private void setUpMediaController() {
+        mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(this);
+        //mediaController.setAnchorView(findViewById(R.id.something));
+        mediaController.setEnabled(true);
+    }
+
+
+    //Media Controller controls
+    @Override
+    public void start() {
+        if(musicService != null && musicBound) {
+            musicService.resumePlayer();
+        }
+    }
+
+    @Override
+    public void pause() {
+        if(musicService != null && musicBound) {
+            musicService.pausePlayer();
+        }
+    }
+
+    @Override
+    public int getDuration() {
+        if(musicService != null && musicBound && musicService.isPlaying()) {
+            return musicService.getSongDuration();
+        }
+        else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if(musicService != null && musicBound && musicService.isPlaying()) {
+            return musicService.getSongPosition();
+        }
+        else {
+            return 0;
+        }
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        if(musicService != null && musicBound) {
+            musicService.seekTo(pos);
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(musicService != null && musicBound) {
+            return musicService.isPlaying();
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }

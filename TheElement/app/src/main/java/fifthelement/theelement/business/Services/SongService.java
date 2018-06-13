aa3d1343be5a0c4e959110f2a6f8ad37edd7f068
Gershon <reydmang@myumanitoba.ca>
2018-06-12 23:28:27 -0500

    Basic Business Logic Persist the music across pages

---
+++ TheElement/app/src/main/java/fifthelement/theelement/business/Services/SongService.java
@@ -0,0 +1,51 @@
+package fifthelement.theelement.business.Services;
+
+import java.util.List;
+
+import fifthelement.theelement.application.Services;
+import fifthelement.theelement.objects.Song;
+import fifthelement.theelement.persistence.SongPersistence;
+
+
+// TODO: Our MainActivity Initializes This - Fragments Will Call
+//       these Methods!
+// TODO: TESTS!
+public class SongService {
+
+    private SongPersistence songPersistence;
+
+    public SongService() {
+        songPersistence = Services.getSongPersistence();
+    }
+
+    public List<Song> getSongs() {
+        return songPersistence.getAllSongs();
+    }
+
+    // TODO: Try-Catch
+    public boolean insertSong(Song song) {
+        return songPersistence.storeSong(song);
+    }
+
+    // TODO: Try-Catch
+    public boolean updateSong(Song song) {
+        return songPersistence.updateSong(song);
+    }
+
+    // TODO: Try-Catch
+    public boolean deleteSong(Song song) {
+        return songPersistence.deleteSong(song);
+    }
+
+    /*
+        Navjots Sort Songs Here
+     */
+
+    /*
+        Tristans Delete Song Stuff Here
+     */
+
+    /*
+        Jeremys Search Here
+     */
+}

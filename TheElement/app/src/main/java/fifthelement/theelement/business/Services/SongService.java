b1c31f25794f2e9d1f37efade68e9199287972dc
Jarde01 <j_arde@live.ca>
2018-06-14 14:01:54 -0500

    fixing persistence issue, comments

--- TheElement/app/src/main/java/fifthelement/theelement/business/Services/SongService.java
+++ TheElement/app/src/main/java/fifthelement/theelement/business/Services/SongService.java
@@ -81,11 +81,8 @@ public class SongService {
         return false;
     }
 
-    /**
-     * This method checks if any song already has the same path
-     * @param path  The path to check
-     * @return  True if there exists a song with the same path
-     */
+    // Method checks if any song already has the same path
+    // and returns true if a songs exists with the same path
     public boolean pathExists(String path) {
         List<Song> songs = getSongs();
         boolean toReturn = false;
@@ -105,7 +102,7 @@ public class SongService {
     }
 
     public List<Song> search(String query) {
-        List<Song> allSongs = songPersistence.getAllSongs();
+        List<Song> allSongs = getSongs();
         ArrayList<Song> matchesList = new ArrayList<>();
         Matcher matcher;
 

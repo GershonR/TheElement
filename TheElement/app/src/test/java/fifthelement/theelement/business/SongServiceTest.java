7a31d274673b4fb72b407393a4437690cec6f2dc
Navjot Dhillon <navjot@Navjots-MBP.hitronhub.home>
2018-06-13 22:50:37 -0500

    SongListAdapter methods changes

--- TheElement/app/src/test/java/fifthelement/theelement/business/SongServiceTest.java
+++ TheElement/app/src/test/java/fifthelement/theelement/business/SongServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import fifthelement.theelement.business.Services.SongService;

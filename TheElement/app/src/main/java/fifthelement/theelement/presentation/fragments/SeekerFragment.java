c463de86e87a742ead08ad192944e63c0d3a9224
AdamB <bouttela@myumanitoba.ca>
2018-06-25 17:53:01 -0500

    Issue#31-Adam: - Removed PlaybackListener as a helper class and moved seekbar updating code to the seekbar fragment. - Added check during fragment rendering to see if music is playing, if so, the seekbar should begin updating.

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SeekerFragment.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SeekerFragment.java
@@ -11,6 +11,10 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import fifthelement.theelement.R;
 import fifthelement.theelement.presentation.activities.MainActivity;
 import fifthelement.theelement.presentation.services.MusicService;
@@ -18,10 +22,14 @@ import fifthelement.theelement.presentation.services.PlaybackInfoListener;
 
 public class SeekerFragment extends Fragment {
 
+    public static final int PLAYBACK_POSITION_REFRESH_INTERVAL_MS = 500;
+
     private SeekBar mSeekbarAudio;
     private MusicService musicService;
     private View view;
     private boolean mUserIsSeeking = false;
+    private ScheduledExecutorService mExecutor;
+    private Runnable mSeekbarPositionUpdateTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +40,6 @@ public class SeekerFragment extends Fragment {
         musicService = ((MainActivity)getActivity()).getMusicService();
         initializeUI();
         initializeSeekbar();
-        initializePlaybackController();
         return view;
     }
 
@@ -45,6 +52,7 @@ public class SeekerFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        stopUpdatingCallbackWithPosition(false);
                         musicService.pause();
                     }
                 });
@@ -52,13 +60,10 @@ public class SeekerFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        startUpdatingCallbackWithPosition();
                         musicService.start();
                     }
                 });
-    }
-
-    private void initializePlaybackController() {
-        musicService.setPlaybackInfoListener(new PlaybackListener());
     }
 
     private void initializeSeekbar() {
@@ -84,18 +89,58 @@ public class SeekerFragment extends Fragment {
                         musicService.seekTo(userSelectedPosition);
                     }
                 });
+
+        //Start updating the seekbar if music is playing
+        if(musicService.isPlaying()){
+            startUpdatingCallbackWithPosition();
+        }
     }
 
-    //Listener that will help seekbars update with the progressing of music playback
-    public class PlaybackListener extends PlaybackInfoListener {
-
-        public void onDurationChanged(int duration) {
-            mSeekbarAudio.setMax(duration);
+    // This function will spawn a scheduled executor to call a UI update function at a set interval.
+    private void startUpdatingCallbackWithPosition() {
+        if (mExecutor == null) {
+            mExecutor = Executors.newSingleThreadScheduledExecutor();
         }
+        if (mSeekbarPositionUpdateTask == null) {
+            mSeekbarPositionUpdateTask = new Runnable() {
+                @Override
+                public void run() {
+                    updateProgressCallbackTask();
+                }
+            };
+        }
+        mExecutor.scheduleAtFixedRate(
+                mSeekbarPositionUpdateTask,
+                0,
+                PLAYBACK_POSITION_REFRESH_INTERVAL_MS,
+                TimeUnit.MILLISECONDS
+        );
+    }
 
-        public void onPositionChanged(int position) {
+    // This function will stop the scheduled executor from calling UI update function and uses a
+    // boolean parameter to determine if seekbar UI elements reporting playback must reset to 0.
+    private void stopUpdatingCallbackWithPosition(boolean resetUIPlaybackPosition) {
+        if (mExecutor != null) {
+            mExecutor.shutdownNow();
+            mExecutor = null;
+            mSeekbarPositionUpdateTask = null;
+            if (resetUIPlaybackPosition) {
+                mSeekbarAudio.setProgress(0);
+            }
+        }
+    }
+
+    // This function will call the setProgress method on the seekbar with the music player's
+    // current position, which will update the position of the seekbar during playback
+    private void updateProgressCallbackTask() {
+        if (musicService != null && musicService.isPlaying()) {
+            int currentPosition = musicService.getCurrentPosition();
+            final int duration = musicService.getDuration();
+            if (mSeekbarAudio.getMax() != duration){
+                mSeekbarAudio.setMax(duration);
+            }
             if (!mUserIsSeeking) {
-                mSeekbarAudio.setProgress(position);
+                mSeekbarAudio.setProgress(currentPosition);
             }
         }
     }

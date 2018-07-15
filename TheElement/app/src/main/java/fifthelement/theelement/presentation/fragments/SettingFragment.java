c6018b252dce383cdec6ad08e8c95ab5e33107ba
Gershon <reydmang@myumanitoba.ca>
2018-07-15 13:34:17 -0500

    Initial Commit

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SettingFragment.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SettingFragment.java
@@ -3,6 +3,8 @@ package fifthelement.theelement.presentation.fragments;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -37,7 +39,7 @@ public class SettingFragment extends Fragment {
 
 
         // Create and populate a List of for the library.
-        String[] options = new String[]{"Theme1", "Theme2", "Theme3", "Delete Songs", "Hide album art notification"};
+        String[] options = new String[]{"Themes", "Delete Songs", "Hide album art notification"};
         ArrayList<String> libraryList = new ArrayList<String>();
         libraryList.addAll(Arrays.asList(options));
 
@@ -46,13 +48,27 @@ public class SettingFragment extends Fragment {
         mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ThemeUtil.changeToTheme(getActivity(), position);
-                System.out.println("Called: " + position);
+                if(position == 0) {
+                    selectTheme();
+                }
             }
         });
 
         // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter(listAdapter);
         return view;
+    }
+
+    private void selectTheme() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Please choose a theme:")
+
+                .setItems(new String[]{"Blue", "Aqua", "Orange"}, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int clicked) {
+                        ThemeUtil.changeToTheme(getActivity(), clicked);
+                    }
+                });
+        builder.create();
+        builder.show();
     }
 }

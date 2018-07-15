a42ab98dc907765ac5be85a0ee5d7196cbf2d18d
AdamB <bouttela@myumanitoba.ca>
2018-07-15 18:34:58 -0500

    Adam/IntegrationTests:
    - Added integration tests for AuthorService seam

--- TheElement/app/src/test/java/fifthelement/theelement/AllIntegrationTests.java
+++ TheElement/app/src/test/java/fifthelement/theelement/AllIntegrationTests.java
@@ -4,12 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import fifthelement.theelement.business.AlbumServiceIT;
+import fifthelement.theelement.business.AuthorServiceIT;
 import fifthelement.theelement.business.SongServiceIT;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         SongServiceIT.class,
         AlbumServiceIT.class,
+        AuthorServiceIT.class,
 })
 
 public class AllIntegrationTests {
---
+++ TheElement/app/src/test/java/fifthelement/theelement/business/AuthorServiceIT.java
@@ -0,0 +1,135 @@
+package fifthelement.theelement.business;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import fifthelement.theelement.application.Persistence;
+import fifthelement.theelement.application.Services;
+import fifthelement.theelement.business.services.AlbumService;
+import fifthelement.theelement.business.services.AuthorService;
+import fifthelement.theelement.objects.Album;
+import fifthelement.theelement.objects.Author;
+import fifthelement.theelement.utils.TestDatabaseUtil;
+
+public class AuthorServiceIT {
+    private AuthorService authorService;
+    private File tempDB;
+
+    @Before
+    public void setUpTestDB() throws IOException {
+        this.tempDB = TestDatabaseUtil.copyDB();
+        authorService = Services.getAuthorService();
+    }
+
+    @Test
+    public void getAllAuthorsTest() {
+        List<Author> authors = authorService.getAuthors(); // Database has 2
+
+        Assert.assertTrue("getAllAuthorsTest: author size != 2", authors.size() == 2);
+    }
+
+    @Test
+    public void insertAuthorValidTest() {
+        Author author = new Author("Jim Bob");
+        authorService.insertAuthor(author);
+
+        Assert.assertTrue("insertAuthorValidTest: author size != 3", authorService.getAuthors().size() == 3);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertAuthorInValidTest() {
+        Author author = null;
+        authorService.insertAuthor(author);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertAuthorDuplicateTest() {
+        Author authorOne = new Author("Jim Bob");
+        authorOne.setUUID(UUID.fromString("493410b3-dd0b-4b78-97bf-289f50f6e74f"));
+        Author authorTwo = new Author("Bob Jim");
+        authorTwo.setUUID(UUID.fromString("493410b3-dd0b-4b78-97bf-289f50f6e74f"));
+
+        authorService.insertAuthor(authorOne);
+        authorService.insertAuthor(authorTwo);
+    }
+
+    @Test
+    public void updateAuthorValidTest() {
+        Author authorOne = new Author("Jim Bob");
+        authorOne.setUUID(UUID.fromString("493410b3-dd0b-4b78-97bf-289f50f6e74f"));
+        Author authorTwo = new Author("Bob Jim");
+        authorTwo.setUUID(UUID.fromString("493410b3-dd0b-4b78-97bf-289f50f6e74f"));
+
+        boolean insertReturn = authorService.insertAuthor(authorOne);
+        boolean updateReturn = authorService.updateAuthor(authorTwo);
+
+        Assert.assertTrue("updateAuthorValidTest: insertReturn != true", insertReturn);
+        Assert.assertTrue("updateAuthorValidTest: updateReturn != true", updateReturn);
+        Assert.assertTrue("updateAuthorValidTest: song size != 4", authorService.getAuthors().size() == 4);
+
+        Author author = authorService.getAuthorByUUID(UUID.fromString("493410b3-dd0b-4b78-97bf-289f50f6e74f"));
+        Assert.assertTrue("updateAuthorValidTest: song name != Changed Author Name", "Bob Jim".equals(author.getName()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateAuthorInValidTest() {
+        Author author = null;
+        authorService.updateAuthor(author);
+    }
+
+    @Test
+    public void updateAuthorNotExistTest() {
+        Author author = new Author("Jim Bob");
+        boolean result = authorService.updateAuthor(author);
+        Assert.assertFalse("updateAuthorNotExistTest: result != false", result);
+        Assert.assertTrue("updateAuthorNotExistTest: author size != 2", authorService.getAuthors().size() == 2);
+
+    }
+
+    @Test
+    public void deleteAuthorValidTest() {
+        Author authorOne = new Author("Santa");
+        UUID authorUUID = UUID.fromString("793410b3-dd0b-4b78-97bf-289f50f6e74f");
+        authorOne.setUUID(authorUUID);
+
+        boolean insertReturn = authorService.insertAuthor(authorOne);
+        Assert.assertTrue("deleteAuthorValidTest: insertReturn != true", insertReturn);
+        Assert.assertTrue("deleteAuthorValidTest: author size != 3", authorService.getAuthors().size() == 3);
+
+        boolean deleteReturn = authorService.deleteAuthor(authorOne);
+        Assert.assertTrue("deleteAuthorValidTest: deleteReturn != true", deleteReturn);
+        Assert.assertTrue("deleteAuthorValidTest: author size != 2", authorService.getAuthors().size() == 2);
+
+        Author deletedAuthor = authorService.getAuthorByUUID(authorUUID);
+        Assert.assertNull("deleteAuthorValidTest: deletedAuthor != null", deletedAuthor);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteAuthorInValidTest() {
+        Author author = null;
+        authorService.deleteAuthor(author);
+    }
+
+    @Test
+    public void deleteAuthorNotExistTest() {
+        Author author = new Author("Pepe");
+        boolean result = authorService.deleteAuthor(author);
+        Assert.assertFalse("deleteAuthorNotExistTest: result != false", result);
+        Assert.assertTrue("deleteAuthorNotExistTest: author size != 2", authorService.getAuthors().size() == 2);
+    }
+
+    @After
+    public void tearDownTestDB() {
+        TestDatabaseUtil.killDB(tempDB);
+        Persistence.resetPersistence();
+        Services.resetServices();
+    }
+}

164631cc8e3a39ba7c8fd1776b7925a7f13c31dc
AdamB <bouttela@myumanitoba.ca>
2018-07-15 18:31:40 -0500

    Adam/IntegrationTests:
    - Added integration tests for AlbumService seam

--- TheElement/app/src/test/java/fifthelement/theelement/AllIntegrationTests.java
+++ TheElement/app/src/test/java/fifthelement/theelement/AllIntegrationTests.java
@@ -3,11 +3,13 @@ package fifthelement.theelement;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import fifthelement.theelement.business.AlbumServiceIT;
 import fifthelement.theelement.business.SongServiceIT;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         SongServiceIT.class,
+        AlbumServiceIT.class,
 })
 
 public class AllIntegrationTests {

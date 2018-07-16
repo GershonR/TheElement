d5849b6fbf66a168638e20c33ce373e5b300a509
Tristan <tristanmontilla@gmail.com>
2018-07-16 13:38:26 -0500

    Deletion fix to reflect Stats page

--- TheElement/app/src/androidTest/java/fifthelement/theelement/presentation/activities/SettingsTest.java
+++ TheElement/app/src/androidTest/java/fifthelement/theelement/presentation/activities/SettingsTest.java
@@ -1,6 +1,7 @@
 package fifthelement.theelement.presentation.activities;
 
 
+import android.os.SystemClock;
 import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
@@ -10,14 +11,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.List;
+
 import fifthelement.theelement.R;
+import fifthelement.theelement.business.exceptions.SongAlreadyExistsException;
+import fifthelement.theelement.objects.Song;
 import fifthelement.theelement.presentation.activities.TestHelpers.AndroidTestHelpers;
 
 import static android.support.test.espresso.Espresso.onData;
@@ -27,6 +34,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static fifthelement.theelement.presentation.activities.TestHelpers.AndroidTestHelpers.childAtPosition;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
@@ -42,9 +51,9 @@ public class SettingsTest {
     public void SelectTheme2Test() {
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Navigate up"),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 allOf(withId(R.id.toolbar),
-                                        AndroidTestHelpers.childAtPosition(
+                                        childAtPosition(
                                                 withClassName(is("android.widget.LinearLayout")),
                                                 0)),
                                 1),
@@ -52,9 +61,9 @@ public class SettingsTest {
         appCompatImageButton.perform(click());
 
         ViewInteraction navigationMenuItemView = onView(
-                allOf(AndroidTestHelpers.childAtPosition(
+                allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
-                                AndroidTestHelpers.childAtPosition(
+                                childAtPosition(
                                         withId(R.id.nvView),
                                         0)),
                         5),
@@ -63,7 +72,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView = onData(anything())
                 .inAdapterView(allOf(withId(R.id.library_view),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.LinearLayout")),
                                 0)))
                 .atPosition(0);
@@ -71,7 +80,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView2 = onData(anything())
                 .inAdapterView(allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.FrameLayout")),
                                 0)))
                 .atPosition(1);
@@ -83,9 +92,9 @@ public class SettingsTest {
     public void selectTheme3Test() {
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Navigate up"),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 allOf(withId(R.id.toolbar),
-                                        AndroidTestHelpers.childAtPosition(
+                                        childAtPosition(
                                                 withClassName(is("android.widget.LinearLayout")),
                                                 0)),
                                 1),
@@ -93,9 +102,9 @@ public class SettingsTest {
         appCompatImageButton.perform(click());
 
         ViewInteraction navigationMenuItemView = onView(
-                allOf(AndroidTestHelpers.childAtPosition(
+                allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
-                                AndroidTestHelpers.childAtPosition(
+                                childAtPosition(
                                         withId(R.id.nvView),
                                         0)),
                         5),
@@ -104,7 +113,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView = onData(anything())
                 .inAdapterView(allOf(withId(R.id.library_view),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.LinearLayout")),
                                 0)))
                 .atPosition(0);
@@ -112,7 +121,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView2 = onData(anything())
                 .inAdapterView(allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.FrameLayout")),
                                 0)))
                 .atPosition(2);
@@ -122,11 +131,14 @@ public class SettingsTest {
 
     @Test
     public void deleteSongsTest() {
+        // Delete one song first and see if Stats Page reflects changes
+        this.deleteSongWithStatsCheck();
+
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Navigate up"),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 allOf(withId(R.id.toolbar),
-                                        AndroidTestHelpers.childAtPosition(
+                                        childAtPosition(
                                                 withClassName(is("android.widget.LinearLayout")),
                                                 0)),
                                 1),
@@ -134,9 +146,9 @@ public class SettingsTest {
         appCompatImageButton.perform(click());
 
         ViewInteraction navigationMenuItemView = onView(
-                allOf(AndroidTestHelpers.childAtPosition(
+                allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
-                                AndroidTestHelpers.childAtPosition(
+                                childAtPosition(
                                         withId(R.id.nvView),
                                         0)),
                         2),
@@ -145,9 +157,9 @@ public class SettingsTest {
 
         ViewInteraction appCompatImageButton2 = onView(
                 allOf(withContentDescription("Navigate up"),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 allOf(withId(R.id.toolbar),
-                                        AndroidTestHelpers.childAtPosition(
+                                        childAtPosition(
                                                 withClassName(is("android.widget.LinearLayout")),
                                                 0)),
                                 1),
@@ -155,9 +167,9 @@ public class SettingsTest {
         appCompatImageButton2.perform(click());
 
         ViewInteraction navigationMenuItemView2 = onView(
-                allOf(AndroidTestHelpers.childAtPosition(
+                allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
-                                AndroidTestHelpers.childAtPosition(
+                                childAtPosition(
                                         withId(R.id.nvView),
                                         0)),
                         5),
@@ -166,7 +178,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView = onData(anything())
                 .inAdapterView(allOf(withId(R.id.library_view),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.LinearLayout")),
                                 0)))
                 .atPosition(1);
@@ -174,7 +186,7 @@ public class SettingsTest {
 
         DataInteraction appCompatTextView2 = onData(anything())
                 .inAdapterView(allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 withClassName(is("android.widget.FrameLayout")),
                                 0)))
                 .atPosition(0);
@@ -182,9 +194,9 @@ public class SettingsTest {
 
         ViewInteraction appCompatImageButton3 = onView(
                 allOf(withContentDescription("Navigate up"),
-                        AndroidTestHelpers.childAtPosition(
+                        childAtPosition(
                                 allOf(withId(R.id.toolbar),
-                                        AndroidTestHelpers.childAtPosition(
+                                        childAtPosition(
                                                 withClassName(is("android.widget.LinearLayout")),
                                                 0)),
                                 1),
@@ -192,14 +204,205 @@ public class SettingsTest {
         appCompatImageButton3.perform(click());
 
         ViewInteraction navigationMenuItemView3 = onView(
-                allOf(AndroidTestHelpers.childAtPosition(
+                allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
-                                AndroidTestHelpers.childAtPosition(
+                                childAtPosition(
                                         withId(R.id.nvView),
                                         0)),
                         2),
                         isDisplayed()));
         navigationMenuItemView3.perform(click());
 
+        // some manual check if there's anything. We should know this would work by now
+        this.checkStatsPage();
+        SystemClock.sleep(1000);
+    }
+
+    private void checkStatsPage() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nvView),
+                                        0)),
+                        5),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        DataInteraction appCompatTextView = onData(anything())
+                .inAdapterView(allOf(withId(R.id.library_view),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)))
+                .atPosition(3);
+        appCompatTextView.perform(click());
+    }
+
+    @Test
+    public void playSongWithSongPlayedCheck() {
+        checkStatsPage();
+        int oldMostPlayed = mActivityTestRule.getActivity().getSongService().getTotalSongPlays();
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nvView),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+        DataInteraction frameLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.song_list_view),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                1)))
+                .atPosition(0);
+        frameLayout.perform(click());
+        frameLayout.perform(click());
+        checkStatsPage();
+        int newMostPlayed = mActivityTestRule.getActivity().getSongService().getTotalSongPlays();
+        Assert.assertTrue(oldMostPlayed+2 == newMostPlayed);
+    }
+
+    @Test
+    public void skipSongWithSongPlayedCheck() {
+        checkStatsPage();
+        int oldMostPlayed = mActivityTestRule.getActivity().getSongService().getTotalSongPlays();
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nvView),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+        DataInteraction frameLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.song_list_view),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                1)))
+                .atPosition(0);
+        frameLayout.perform(click());
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.button_next),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.constraint.ConstraintLayout")),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+        appCompatImageButton2.perform(click());
+        checkStatsPage();
+        int newMostPlayed = mActivityTestRule.getActivity().getSongService().getTotalSongPlays();
+        Assert.assertTrue(oldMostPlayed+1 == newMostPlayed);
+    }
+
+    private void deleteSongWithStatsCheck() {
+        checkStatsPage();
+        int totalSongsOwned = mActivityTestRule.getActivity().getSongService().getSortedSongListByMostPlayed().size();
+        Song oldMostPlayedSong = mActivityTestRule.getActivity().getSongService().getMostPlayedSong();
+
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nvView),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(first(allOf(withId(R.id.popup_button),
+                        isDisplayed()))));
+        appCompatImageButton2.perform(click());
+
+        Assert.assertTrue("Song Size Is Not 4", mActivityTestRule.getActivity().getSongService().getSongs().size() == 4);
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("Delete Song"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.v7.view.menu.ListMenuItemView")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        checkStatsPage();
+        SystemClock.sleep(1000);
+        int newTotalSongsOwned = mActivityTestRule.getActivity().getSongService().getSortedSongListByMostPlayed().size();
+        Song newMostPlayedSong = mActivityTestRule.getActivity().getSongService().getMostPlayedSong();
+        Assert.assertTrue(totalSongsOwned-1 == newTotalSongsOwned);
+        Assert.assertNotNull("No song to assert with. Empty list?",oldMostPlayedSong);
+        Assert.assertNotEquals(oldMostPlayedSong, newMostPlayedSong);
+    }
+
+    private <T> Matcher<T> first(final Matcher<T> matcher) {
+        return new BaseMatcher<T>() {
+            boolean isFirst = true;
+
+            @Override
+            public boolean matches(final Object item) {
+                if (isFirst && matcher.matches(item)) {
+                    isFirst = false;
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("should return first matching item");
+            }
+        };
     }
 }

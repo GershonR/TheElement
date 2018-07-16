4fd25a8925331355572d1ab7062b20a2d889e9af
AdamB <bouttela@myumanitoba.ca>
2018-07-15 20:26:31 -0500

    Adam/IntegrationTests:
    - Deleted unused exception classes

--- TheElement/app/src/main/java/fifthelement/theelement/business/exceptions/AlbumAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package fifthelement.theelement.business.exceptions;
-
-public class AlbumAlreadyExistsException extends Exception {
-
-    public AlbumAlreadyExistsException(String id) {
-        super("Album with ID: " + id + " already exists");
-    }
-
-}

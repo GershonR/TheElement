14efdf34ef04cef35acaf978fbd4c43530a51823
AdamB <bouttela@myumanitoba.ca>
2018-07-15 22:00:51 -0500

    Adam/ClearLibrarySettingOption:
    - Merged in master changes and resolved conflicts

--- TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SettingFragment.java
+++ TheElement/app/src/main/java/fifthelement/theelement/presentation/fragments/SettingFragment.java
@@ -33,9 +33,6 @@ public class SettingFragment extends Fragment {
     private ListView mainListView;
     private ArrayAdapter<String> listAdapter;
 
-    public final static int THEME_HIGH_VAL = 2;
-    public final static int DELETE_SONGS = 3;
-
     /**
      * Called when the activity is first created.
      */
@@ -49,7 +46,6 @@ public class SettingFragment extends Fragment {
 
 
         // Create and populate a List of for the library.
-        String[] options = new String[]{"Theme1", "Theme2", "Theme3", "Delete Songs"};
         ArrayList<String> libraryList = new ArrayList<String>();
         libraryList.addAll(Arrays.asList(SettingsConstants.SETTING_OPTIONS));
 
@@ -61,10 +57,7 @@ public class SettingFragment extends Fragment {
                 if(position == 0) {
                     selectTheme();
                 }
-                if(position <= THEME_HIGH_VAL) {
-                    ThemeUtil.changeToTheme(getActivity(), position);
-                    System.out.println("Called: " + position);
-                } else if(position == DELETE_SONGS){
+                else if(position == 1){
                     SongService songService = Services.getSongService();
                     SongListService songListService = Services.getSongListService();
                     songService.clearAllSongs();
